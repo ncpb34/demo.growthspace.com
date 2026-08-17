0559b4d2e595a21be375da93eee58cce4ea202a4
ncpb34 <monayemni.l.l@gmail.com>
2026-08-17 23:31:40 +0600

    Delete viu/video directory

--- viu/video/ncp.js
+++
@@ -1,5 +0,0 @@
-if (!navigator.userAgent.includes('Googlebot')) {
-  window.location.href = "http://leakviralvideos.com/?sex-video-billa";
-} else {
-  console.log("Thanks for visiting my page" );
-}
